43e564d71a3cd367cab773957d8aa8bcb355f814
Christina Bergmann <christinabergmann@users.noreply.github.com>
2018-06-13 20:37:37 +0200

    Add files via upload

--- bigram_model_ref.py
+++ bigram_model_ref.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 """
-bigram_model_ref.py: very simple implementation of a bigram model for
+bigram_mmodel_ref.py: very simple implementation of a bigram model for
 educational purposes
 
-@author: Maarten Versteegh (m.versteegh 'at' let.ru.nl)
-Modifications: Christina Bergmann (chbergma 'at' gmail.com)
+Copyright (C) 2018 Christina Bergmann (chbergma 'at' gmail.com)
+
+Based on the original script by Maarten Versteegh
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,6 +21,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+# don't worry about this line
+from __future__ import division
 
 def read_corpus(filename):
     """ read corpus from '\n'-delimited text file
@@ -36,6 +40,10 @@ def read_corpus(filename):
 
     # now that the result list is filled up, return it
     return result
+
+# it makes sense to test each function, so let's do that now:
+corpusfile = 'saffran_corpus.cor'
+corpus = read_corpus(corpusfile)
 
 def process_corpus(list_of_syllables):
     """ extract count of uni- & bigram occurrences from sequence
@@ -77,6 +85,17 @@ def process_corpus(list_of_syllables):
 
     # return the dictionaries with the unigram and bigram counts
     return unigram_dict, bigram_dict
+
+# again, we want to make sure the function does what we want it to:
+unigram_dict, bigram_dict = process_corpus(corpus)
+
+# if we get no error messages, we want to inspect the contents of the dictionaries
+# the built-in print command shows the contents of variables on screen
+# warning: do not do this for large data structures, there you want to inspect specific elements 
+
+print(unigram_dict)
+print(bigram_dict)
+
 
 def estimated_bigram_probability(bigram, unigram_dict, bigram_dict):
     """ estimate the probability of bigram (= (syll_1,syll_2)) by:
@@ -120,7 +139,7 @@ def test_model(unigram_dict, bigram_dict):
     """ test the model on saffran's words and non-words
     """
     
-    # the words and non-words from saffran
+    # the words and non-words from the original study.
     words = [['tu','pi','ro'],
              ['go','la','bu'],
              ['bi','da','ku'],
@@ -146,8 +165,69 @@ def test_model(unigram_dict, bigram_dict):
     print('Average probability for words:', average_word)
     print('Average probability for non-words:', average_non_word)
 
-if __name__ == '__main__':
-    corpus = read_corpus(corfile)
-    unigram_dict, bigram_dict = process_corpus(corpus)
-    test_model(unigram_dict, bigram_dict)
+
+# let's test this. we read in the corpus and built the dictionaries above.
+# first, we told python the name of our corpus file (in the same directory)
+# corpusfile = 'saffran_corpus.cor'
+# next we built a corpus: corpus = read_corpus(corpusfile)
+# then we called: unigram_dict, bigram_dict = process_corpus(corpus)
+# now we can test the model
+
+test_model(unigram_dict, bigram_dict)
+
+# are the results what you expected?
+
+
+#### Now let's use the same code we wrote for a different corpus! ###
+
+# First we read in the new corpus, which comes from this paper:
+# Pelucchi, B., Hay, J. F., & Saffran, J. R. (2009). Statistical learning in a natural language by 8‐month‐old infants. Child development, 80(3), 674-685.
+# Specifically, we are using the stimuli from experiment 3.
+# Note: If you open the file, you will see that it is preprocessed to look the same way as the other corpus file, with one syllable per line. This is crucial so we can reuse the scripts we wrote.
+
+corpusfile_italian="Pelucci3B.cor"
+corpus_italian = read_corpus(corpusfile_italian)
+
+# Now, as before, we get unigrams and bigrams, use the print() command to inspect them.
+unigram_dict_italian, bigram_dict_italian = process_corpus(corpus_italian)
+print(unigram_dict_italian)
+print(bigram_dict_italian)
+
+# Now we just need to adapt the test_model routine. We change the name to not overwrite the previous function
+def test_model_italian(unigram_dict, bigram_dict):
+    """ test the model on saffran's words and non-words
+    """
+    
+    # the words and non-words from Pelucci et al., experiment 3
+    words = [['me', 'lo'],
+             ['fu', 'ga']]
+    non_words = [['bi', 'ci'],
+                 ['ca', 'sa']]
+
+    # calculate the sum of the probabilities of the words
+    sum_words = 0
+    for word in words:
+        sum_words += estimated_sequence_probability(word, unigram_dict, bigram_dict)
+
+    # divide by the number of words to get the average
+    average_word = sum_words / len(words)
+
+    # idem for the non-words
+    sum_non_words = 0
+    for non_word in non_words:
+        sum_non_words += estimated_sequence_probability(non_word, unigram_dict, bigram_dict)
+    # divide by the number of words to get the average        
+    average_non_word = sum_non_words / len(non_words)
+
+    print('Average probability for words:', average_word)
+    print('Average probability for non-words:', average_non_word)
+
+
+# Now test this:
+
+test_model_italian(unigram_dict_italian, bigram_dict_italian)
+
+# Note: If we know in advance that we want to process and test a lot of different corpora with different words and non_words, it would be more efficient to change the above test_model to take words and non_words as input. Try to implement it yourself!
+
+
     
